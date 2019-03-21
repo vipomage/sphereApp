bc5a914582c7fcd87884cff1491819f198d0bc23
Eray <vipomage@gmail.com>
2019-03-21 16:11:24 +0200

    app.component.html refactored to reflect changes typo fix SphereService initialization of spheres refactored logAction function separated from Scales findBetweenThree function separated from Scales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-trailing-whitespace */
 import { Component, OnInit } from '@angular/core';
-import { SphereServiceService } from './sphere-service.service';
-import { Scales } from './scales';
+import { SphereService } from './sphere-service.service';
+import { Result, Scales } from './scales';
 import { Sphere } from './sphere';
 
 @Component({
@@ -10,25 +10,68 @@ import { Sphere } from './sphere';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public spheres: Sphere[] = this.sphereService.generateSpheres();
-  public scales: Scales = new Scales(
-    this.spheres.slice(0, 3),
-    this.spheres.slice(3, 6)
-  );
-  public result: string;
+  public spheres: Sphere[];
+  public scales: Scales;
+  public output: string;
+  public actions: string[];
 
-  constructor(private sphereService: SphereServiceService) {}
+  constructor(private sphereService: SphereService) {}
 
   public find = () => {
-    if (this.result) {
-      this.spheres = this.sphereService.generateSpheres();
-      this.scales = new Scales(
-        this.spheres.slice(0, 3),
-        this.spheres.slice(3, 6)
-      );
-      this.result = this.scales.compareSides();
+    this.actions = [];
+    this.logAction('Generating Random 7 Spheres!');
+    this.spheres = this.sphereService.generateSpheres();
+    this.logAction('Instantiating Scales');
+    this.scales = new Scales();
+    this.logAction('Splitting random spheres set into two [1,2,3] [4,5,6]');
+    let leftNRightSubSets = {
+      ['left']: this.spheres.slice(0, 3),
+      ['right']: this.spheres.slice(3, 6)
+    };
+    this.logAction('Comparing Left and Right subset');
+    let result = this.scales.compareSides(
+      leftNRightSubSets.left,
+      leftNRightSubSets.right
+    );
+    if (result !== Result.Equal) {
+      this.logAction(`${Result[result]} side is heavier`);
+      if (result === Result.Left) {
+        this.logAction('Comparing left side spheres [1,2,3]');
+        this.output = this.findBetweenThree(leftNRightSubSets.left);
+      } else {
+        this.output = this.findBetweenThree(leftNRightSubSets.right, true);
+      }
+    } else {
+      this.output =
+        'They are Equal therefore sphere number 7 is the special one';
     }
-    this.result = this.scales.compareSides();
+  };
+
+  // Never in my life i hated a function this much
+  private logAction = (action: string): void => {
+    this.actions.push(action);
+  };
+
+  private findBetweenThree =
+    (sphereTriplet: Sphere[],rightSide?: boolean): string => {
+    const left = sphereTriplet[0];
+    const right = sphereTriplet[1];
+    let heaviestOrderNo;
+    this.logAction(`Comparing sphere ${rightSide ? '4' : '1'} to ${rightSide ? '5' : '2'}`);
+    const result = this.scales.compareSides(left, right);
+    if (result === Result.Equal) {
+      this.logAction('They are equal');
+      heaviestOrderNo = 3;
+    } else {
+      if (result === Result.Left) {
+        heaviestOrderNo = 1;
+      } else {
+        heaviestOrderNo = 2;
+      }
+    }
+    return `Sphere number ${
+      rightSide ? heaviestOrderNo + 3 : heaviestOrderNo
+    } is heaviest!`;
   };
 
   ngOnInit(): void {}
