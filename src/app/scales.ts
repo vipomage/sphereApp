c5e520d013e8d36a5cb256240e7af131af9b69aa
Eray <vipomage@gmail.com>
2019-03-21 16:17:10 +0200

    export enum Result unnecessary properties removed Scale became "agnostic" comparison subsets removed form constructor - constructor removed removed findBetweenThree() removed logAction() compareSides() simplified to do one thing only (weight in left - right) calcSum() refactored to work with Sphere | Sphere[]

--- src/app/scales.ts
+++ src/app/scales.ts
@@ -1,96 +1,27 @@
 import { Sphere } from './sphere';
 
-enum Result {
+export enum Result {
   Equal,
   Left,
   Right
 }
 
 export class Scales {
-  private readonly firstTriplet: Sphere[];
-  private readonly secondTriplet: Sphere[];
-  public actions: string[] = [];
-  private result: Result;
-  private output: string;
-
   private leftSideValue: number;
   private rightSideValue: number;
 
-  constructor(firstTriplet: Sphere[], secondTriplet: Sphere[]) {
-    this.firstTriplet = firstTriplet;
-    this.secondTriplet = secondTriplet;
-  }
+  public compareSides = (leftSide: Sphere[] | Sphere, rightSide: Sphere[] | Sphere ): Result => {
+    this.leftSideValue = this.calcSum(leftSide);
+    this.rightSideValue = this.calcSum(rightSide);
 
-  // Determines the heavy sphere among last three
-  private findBetweenThree = (
-    sphereTriplet: Sphere[],
-    second?: boolean
-  ): string => {
-    const left = sphereTriplet[0].weight;
-    const right = sphereTriplet[1].weight;
-    let heaviestOrderNo;
-    this.logAction(
-      `Comparing ${second ? 'fourth' : 'first'} to ${
-        second ? 'fifth' : 'second'
-      }`
-    );
-    if (left === right) {
-      this.logAction(
-        `${second ? 'fourth' : 'first'} and ${
-          second ? 'fifth' : 'second'
-        } are equal!`
-      );
-      heaviestOrderNo = 3;
-    } else {
-      if (left > right) {
-        this.logAction(`${second ? 'fourth' : 'first'} is heavier!`);
-        heaviestOrderNo = 1;
-      } else {
-        this.logAction(`${second ? 'fifth' : 'second'} is heavier!`);
-        heaviestOrderNo = 2;
-      }
+    if (this.leftSideValue !== this.rightSideValue) {
+      return this.leftSideValue > this.rightSideValue
+        ? Result.Left
+        : Result.Right;
     }
-    return `Sphere number ${
-      second ? heaviestOrderNo + 3 : heaviestOrderNo
-    } is heaviest!`;
+    return Result.Equal;
   };
 
-  // Calculates the sum of given subset Sphere[]
-  private calcSum = (sphereArr: Sphere[]): number =>
-    sphereArr.reduce((prev, current) => prev + current.weight, 0);
-
-  // inserts given action to actions []
-  private logAction = (action: string): void => {
-    this.actions.push(action);
-  };
-
-  // Compares first subset of spheres[] to second subset of spheres[]
-  public compareSides = (): string => {
-    this.actions = [];
-    this.leftSideValue = this.calcSum(this.firstTriplet);
-    this.rightSideValue = this.calcSum(this.secondTriplet);
-
-    this.logAction('Comparing [1,2,3] to [4,5,6]');
-
-    if (this.leftSideValue === this.rightSideValue) {
-      this.result = Result.Equal;
-      this.logAction('Both sides are equal!');
-    } else {
-      if (this.leftSideValue > this.rightSideValue) {
-        this.result = Result.Left;
-        this.logAction('Left side is heavier');
-      } else {
-        this.result = Result.Right;
-        this.logAction('Right side is heavier');
-      }
-    }
-    if (this.result === Result.Equal) {
-      this.output = 'Sphere number 7 is heaviest';
-    } else if (this.result === Result.Left) {
-      this.output = this.findBetweenThree(this.firstTriplet);
-    } else if (this.result === Result.Right) {
-      this.output = this.findBetweenThree(this.secondTriplet, true);
-    }
-    return this.output;
-  };
+  private calcSum = (...spheres): number =>
+     spheres.reduce((prev, current) => prev + current.weight, 0);
 }
