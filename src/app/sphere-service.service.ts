e60f23bf18d72d2038765fb7d2115472d2c2d259
Eray <vipomage@gmail.com>
2019-03-21 16:18:26 +0200

    ClassName typo fix getRandomNum() param type added generateSpheres() refactored to reflect changes on Sphere

--- src/app/sphere-service.service.ts
+++ src/app/sphere-service.service.ts
@@ -4,24 +4,21 @@ import { Sphere } from './sphere';
 @Injectable({
   providedIn: 'root'
 })
-export class SphereServiceService {
+export class SphereService {
   private spheres: Sphere[] = [];
-  constructor() {}
-
   // Returns a random number between 0 - (max) is exclusive
-  getRandomNum = (max): number => Math.floor(Math.random() * Math.floor(max));
-
+  static getRandomNum = (max: number): number => Math.floor(Math.random() * Math.floor(max));
   // Generates 7 Spheres with one random with extra weight
   generateSpheres = (): Sphere[] => {
     if (this.spheres) {
       this.spheres = [];
     }
-    const randomNum: number = this.getRandomNum(7);
+    const randomNum: number = SphereService.getRandomNum(7);
     for (let i = 0; i < 7; i++) {
       if (i === randomNum) {
-        this.spheres.push({ weight: 1.1 });
+        this.spheres.push(new Sphere(1.1));
       } else {
-        this.spheres.push({ weight: 1 });
+        this.spheres.push(new Sphere(1));
       }
     }
     return this.spheres;
